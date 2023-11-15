09a5e9dd548749d09bf1b9eb55035ac00f636243
trujillotheo <theotrujillo@hotmail.fr>
2023-11-15 09:43:53 +0100

    comit

---
+++ main2.js
@@ -0,0 +1,5 @@
+var string = "toto";
+var num = 100;
+var tab = ['Apple','Banana'];
+var objet = window;
+alert(string+num+tab[0]+objet.location.pathname);
---
+++ main3.js
@@ -0,0 +1,7 @@
+var string = "toto";
+var num = 100;
+var tab = ['Apple','Banana'];
+var objet = window;
+var w = window.innerWidth;
+var h = window.innerHeight;
+alert(w+", "+h);
